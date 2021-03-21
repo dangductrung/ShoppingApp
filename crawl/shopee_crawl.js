9f59153daf9afaebe9997efd66318374b954d07f
Trung Dang <2017.12ch.34.dangductrung@gmail.com>
2021-03-22 03:44:50 +0700

    Merge branch '1-crawl-link' into main

--- crawl/shopee_crawl.js
+++ crawl/shopee_crawl.js
@@ -1,5 +1,5 @@
 const crawler = require('./crawl');
-const shopee_target = "https://shopee.vn";
+const shopee_target = "https://shopee.vn/-SI%C3%8AU-R%E1%BA%BA-Rule-1-Th%E1%BB%B1c-Ph%E1%BA%A9m-T%C4%83ng-C%C6%A1-5lbs-i.48291728.9207077746";
 const shopee_base_url = "https://shopee.vn";
 const fs = require('fs');
 
