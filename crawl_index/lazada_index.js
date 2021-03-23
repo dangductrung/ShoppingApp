1f7cf2256c81bb349425a34ad588dd15b63081e2
Dang Duc Trung <dangductrung1999@gmail.com>
2021-03-23 13:12:31 +0700

    update server

--- crawl_index/lazada_index.js
+++ crawl_index/lazada_index.js
@@ -6,7 +6,7 @@ const schedule = require('../schedule/lazada_schedule');
 
 require('events').EventEmitter.prototype._maxListeners = 100;
 
-const enviromentName = "dev"
+const enviromentName = "LAZADA_CRAWL"
 app.use(morgan(enviromentName));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,13 +14,14 @@ const port = process.env.PORT || 5001
 
 var server=app.listen(4001, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
-  schedule.crawl();
+});
 
-  app.use(function (err, req, res, next) {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-  })
+schedule.crawl();
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
 
   
   module.exports = app;
---
+++ schedule/lazada_schedule.js
@@ -0,0 +1,29 @@
+//const schedule = require('node-schedule');
+const lazadaCrawl = require('../crawl/lazada_crawl');
+
+const crawl = async () => {
+    try {
+        lazadaCrawl.lazada_crawl();
+      } catch(e) {
+        console.log(e);
+      }
+      
+    // const rule = new schedule.RecurrenceRule();
+    // Sunday &  Wednesday
+    // rule.dayOfWeek = [0, 3];
+    // rule.hour = 0;
+    // rule.minute = 0;
+
+    // const job = schedule.scheduleJob(rule,async function(){
+    //     try {
+    //          shopeeCrawl.shopee_crawl();
+    //          tikiCrawl.tiki_crawl();
+    //          lazadaCrawl.lazada_crawl();
+    //       } catch(e) {
+    //         console.log(e);
+    //       }
+    //   });  
+};
+
+
+module.exports = { crawl }
